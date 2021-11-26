1b2f04fc44f7d7bcb052dd0732d4bc025994fff5
Bogdan Derii <derii.bogdan@gmail.com>
2021-11-26 15:40:56 +0200

    Create task-3.js

---
+++ sprint_1/task-3.js
@@ -0,0 +1,24 @@
+/*
+	Task - 3.
+	Implement the longestLogin(loginList) function, which takes an array of user logins loginList  and returns the longest login. 
+  If the logins of the same length are the longest in the array, the login element with the largest index is returned. 
+  Tip: You can use the reduce() method to solve the task.
+	
+	Function examples:
+	longestLogin(["serg22", "tester_2", "Prokopenko", "guest"]);   //  Prokopenko
+	longestLogin(["user1", "user2", "333", "user4", "aa"]);   //  user4
+*/
+
+function longestLogin(loginList) {
+	const findMaxLengthLogin = loginList.reduce(
+		function (acc, item) {
+			if (acc.length > item.length) {
+				return acc
+			}
+			else { return item }
+		}
+	)
+	return findMaxLengthLogin
+}
+console.log(longestLogin(["serg22", "tester_2", "Prokopenko", "guest"])); //  Prokopenko
+console.log(longestLogin(["user1", "user2", "333", "user4", "aa"])); //  user4
