7a0b5157657bd36f2aff9812d248ac7ce6c275d0
Bogdan Derii <derii.bogdan@gmail.com>
2021-11-29 19:34:30 +0200

    Update task-2.js

--- sprint_1/task-2.js
+++ sprint_1/task-2.js
@@ -6,12 +6,21 @@
 	combineArray([12, "User01", 22, true, -8], ["Index", 6, null, 15]));  // [12, 22, -8, 6, 15]
 */
 
+/*
 function isNumber(n) {
 	return Number(n) === n;
 }
+
 function combineArray(arr1, arr2) {
 	let digitArray1 = arr1.filter((arr1) => isNumber(arr1));
 	let digitArray2 = arr2.filter((arr2) => isNumber(arr2));
 	return digitArray1.concat(digitArray2)
+}
+console.log(combineArray([12, "User01", 22, true, -8], ["Index", 6, null, 15]));
+*/
+
+function combineArray(arr1, arr2) {
+	const newArr = arr1.concat(arr2);
+	return newArr.filter((elem) => typeof (elem) === 'number');
 }
 console.log(combineArray([12, "User01", 22, true, -8], ["Index", 6, null, 15])); // [12, 22, -8, 6, 15]
