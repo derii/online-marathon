ac8681e104044cff702095b558cbc5e8e14f844c
Bogdan Derii <derii.bogdan@gmail.com>
2021-12-08 13:18:31 +0200

    Update task-3.js

--- sprint_4/task-3.js
+++ sprint_4/task-3.js
@@ -1,1 +1,17 @@
+/*
+	Task - 3.
+	Implement the getAge() function to get user age. To find his age you need to call a getUser() async function that
+	return a user object in format {role: "somerole", id: 1}. To get the actual user info you need to call another
+	function getUserProfile(id), that uses id returned from the previous function and return user info as an object
 
+	{name: "Petro", age: 15}. The getAge() must return the age of the user.
+*/
+
+//const { getUser, getUserProfile } = require('./Helper.js');
+
+async function getAge() {
+	const user = await getUser();
+	const userProfile = await getUserProfile(user.id);
+	const age = userProfile.age;
+	return age;
+}
