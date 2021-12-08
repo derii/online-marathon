0f9525037230b30803223040957c324ad37eb1ef
Bogdan Derii <derii.bogdan@gmail.com>
2021-12-08 07:28:23 +0200

    Create task-1.js

---
+++ sprint_4/task-1.js
@@ -0,0 +1,18 @@
+/*
+	Task - 1.
+	Implement the getPromise(delay, message) function, which takes an integer number delay  (between 0 and 2000) and string message and returns a Promise that waits 
+	for specified amount of time (using delay argument) and resolves with the message.
+*/
+
+function getPromise(delay, message) {
+	return new Promise((resolve, reject) => {
+		if ((delay >= 0) && (delay <= 2000)) {
+			setTimeout(() => {
+				resolve(message)
+			}, delay);
+		}
+		else {
+			reject(error)
+		}
+	})
+}
